6c67cfdceff63fe3c6e8e7f1bb41dc9f6eb7d011
kartikd2003 <165130007+kartikd2003@users.noreply.github.com>
2024-11-26 11:51:49 +0530

    Create que3.cpp

---
+++ que3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[5] = {1,5,8,12,18};
+    int b[7] = {3,5,6,11,13,15,30};
+    
+    int c[12];
+    int i=0,j=0,k=0;
+    
+    while(i<5&&j<7){
+        if(a[i]<b[j]){
+            c[k]=a[i];
+            i++;
+            k++;
+        }
+        
+        else{
+            c[k]=b[j];
+            j++;
+            k++;        
+        }
+    }
